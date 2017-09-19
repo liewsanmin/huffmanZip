5947b7016d9a7933d16cb607d0b1725b4c6c8fbd
evanyui <evanyui@hotmail.com>
2017-09-19 02:30:52 -0400

    Create project platform

---
+++ src/main/java/HuffmanZip.java
@@ -0,0 +1,9 @@
+public class HuffmanZip {
+    public HuffmanZip() {
+    
+    }
+
+    public static void main(String args[]) {
+    
+    }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    private int frequency;
+    private char value;
+    public Node(char value) {
+       this.value = value; 
+    }
+    
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public char getValue() {
+        return this.value;
+    }
+    
+    public int getFrequency() {
+        return this.value;
+    }
+}
